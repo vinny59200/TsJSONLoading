99e06e73de5575a8b60d1e3355c8c47bc7bacfd6
DESKTOP-063BMPJ\Vinny <vvauban@gmail.com>
2021-05-07 02:19:17 +0200

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,40 @@
+import { OnInit } from '@angular/core';
 import { Component } from '@angular/core';
+import variableName from '../assets/vv.json';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+
+//https://stackoverflow.com/questions/47206924/angular-5-service-to-read-local-json-file
+export class AppComponent implements OnInit {
+
+  vvobj: any = variableName;
+  
   title = 'VVLOCALJSONLOADING';
+
+  ngOnInit(): void {
+
+    console.log(this.vvobj);
+    
+    //additionnally manipulate the JSON
+    let rewrittenvvobj = rewriteObjectVV1(this.vvobj, "vv1", [{"yay":"yay1"},{"yay":"yay1"},{"yay":"yay1"}]);
+    console.log(rewrittenvvobj);
+
+  }
+
+}
+
+function rewriteObjectVV1(obj, key, val) {
+  let result={...obj}; ;
+  var objects = [];
+  for (var i in result) {
+    if (i == key) {
+      result[i] = val;
+      objects.push(result);
+    }
+  }
+  return objects[0];
 }
